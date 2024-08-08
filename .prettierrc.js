96dac78c0f3f917f0294be9313cb2b271c77f0ff
Pablo Brodt <pablobrodt@gmail.com>
2024-08-07 21:49:19 -0300

    chore: Adiciona regras de lint, formatação e dependencias de teste

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,8 @@
 module.exports = {
-  arrowParens: 'avoid',
-  bracketSameLine: true,
-  bracketSpacing: false,
+  arrowParens: 'always',
+  bracketSameLine: false,
+  bracketSpacing: true,
   singleQuote: true,
   trailingComma: 'all',
+  jsxSingleQuote: true,
 };
