06101da38c7672ad2b48cf270d5f337355c9f90e
Pablo Brodt <pablobrodt@gmail.com>
2024-08-10 02:25:41 -0300

    chore: Adiciona regra para ignorar implementacao de mock dos testes unitários

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,8 @@
 module.exports = {
   preset: 'react-native',
   setupFilesAfterEnv: ['./.jest/jest-setup.ts'],
+  testPathIgnorePatterns: ['/node_modules/'],
+  coveragePathIgnorePatterns: [
+    '<rootDir>/src/modules/common/services/http/mock/http.service.mock.ts',
+  ],
 };
