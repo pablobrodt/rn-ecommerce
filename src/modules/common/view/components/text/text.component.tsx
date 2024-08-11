4fc48871cfed37b548f704e6d932f01aabd6c4a6
Pablo Brodt <pablobrodt@gmail.com>
2024-08-11 03:49:42 -0300

    feat: Adiciona prop para elipse no componente Text

--- src/modules/common/view/components/text/text.component.tsx
+++ src/modules/common/view/components/text/text.component.tsx
@@ -5,8 +5,20 @@ import { PropsWithStyle } from '@common/view/types/props-with-style';
 
 type TextProps = {
   children: string | string[];
+  ellipsis?: boolean;
 } & PropsWithStyle<TextStyle>;
 
-export function Text({ children, style }: TextProps) {
-  return <RNText style={[styles.text, style]}>{children}</RNText>;
+export function Text({ children, style, ellipsis }: TextProps) {
+  const numberOfLines = ellipsis ? 1 : undefined;
+  const ellipsizeMode = ellipsis ? 'tail' : undefined;
+
+  return (
+    <RNText
+      style={[styles.text, style]}
+      numberOfLines={numberOfLines}
+      ellipsizeMode={ellipsizeMode}
+    >
+      {children}
+    </RNText>
+  );
 }
