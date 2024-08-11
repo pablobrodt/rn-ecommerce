5d4ba86b11d78bf1c3c17996d1b244320177de80
Pablo Brodt <pablobrodt@gmail.com>
2024-08-11 00:10:09 -0300

    test: Ignora a store Cart da cobertura de testes unitários

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,7 @@ module.exports = {
   setupFilesAfterEnv: ['./.jest/jest-setup.ts'],
   testPathIgnorePatterns: ['/node_modules/'],
   coveragePathIgnorePatterns: [
+    '<rootDir>/src/modules/cart/store/cart/cart.store.ts',
     '<rootDir>/src/modules/common/services/http/mock/http.service.mock.ts',
   ],
 };
