6132cbd15a2e4153d3abd28c31c02ce21c2ea62e
Pablo Brodt <pablobrodt@gmail.com>
2024-08-11 23:24:52 -0300

    refactor: Troca o indicador de novo produto por indicador de quantidade de produtos

--- src/modules/cart/view/components/cart-button/cart-button.component.tsx
+++ src/modules/cart/view/components/cart-button/cart-button.component.tsx
@@ -1,8 +1,8 @@
-import { useEffect, useRef, useState } from 'react';
 import { View } from 'react-native';
 import { useCartViewModel } from '@cart/viewmodel/cart/cart.viewmodel';
 import { Button } from '@common/view/components/button/button.component';
 import { Colors } from '@common/view/constants/colors';
+import { Text } from '@common/view/components/text/text.component';
 import { CartShopping } from '@assets/icons';
 import { CartRoute } from '@cart/view/route/cart.route';
 
@@ -13,32 +13,28 @@ type CartButtonProps = {
 };
 
 export function CartButton({ navigation }: CartButtonProps) {
-  const [hasNewProducts, setHasNewProducts] = useState(false);
-  const totalProducts = useRef<number>(0);
   const cartViewModel = useCartViewModel();
-
-  useEffect(() => {
-    if (totalProducts.current < cartViewModel.productCount) {
-      setHasNewProducts(true);
-    }
-    totalProducts.current = cartViewModel.productCount;
-  }, [cartViewModel.productCount]);
 
   function goToCart() {
     navigation.navigate(CartRoute.CART);
-    setHasNewProducts(false);
   }
 
-  function renderNewProductsIndicator() {
-    if (hasNewProducts) {
-      return <View style={styles.indicator} />;
+  function renderProductCount() {
+    if (cartViewModel.productCount > 0) {
+      return (
+        <View style={styles.indicator}>
+          <Text style={styles.productQuantity} ellipsis>
+            {cartViewModel.productCount.toString()}
+          </Text>
+        </View>
+      );
     }
   }
 
   return (
     <Button onPress={goToCart} style={styles.container}>
       <CartShopping style={styles.icon} color={Colors.HIGHLIGHT_REGULAR} />
-      {renderNewProductsIndicator()}
+      {renderProductCount()}
     </Button>
   );
 }
--- src/modules/cart/view/components/cart-button/cart-button.style.ts
+++ src/modules/cart/view/components/cart-button/cart-button.style.ts
@@ -1,6 +1,9 @@
+import { Colors } from '@common/view/constants/colors';
 import { StyleSheet } from 'react-native';
 
 const ICON_SIZE = 26;
+const INDICATOR_SIZE = 16;
+const INDICATOR_RADIUS = INDICATOR_SIZE / 2;
 
 export const styles = StyleSheet.create({
   container: {
@@ -9,13 +12,19 @@ export const styles = StyleSheet.create({
     position: 'relative',
   },
   indicator: {
-    width: 10,
-    height: 10,
-    borderRadius: 5,
-    backgroundColor: 'red',
+    width: INDICATOR_SIZE,
+    height: INDICATOR_SIZE,
+    borderRadius: INDICATOR_RADIUS,
+    backgroundColor: Colors.HIGHLIGHT_REGULAR,
     position: 'absolute',
     top: 6,
     right: 6,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  productQuantity: {
+    color: Colors.WHITE,
+    fontWeight: 'bold',
   },
   icon: {
     width: ICON_SIZE,
