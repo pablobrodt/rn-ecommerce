eef28c935ad26cc5576a931b501fe35a461aedce
Pablo Brodt <pablobrodt@gmail.com>
2024-08-11 22:22:33 -0300

    feat: Adiciona iconde Delete

--- src/assets/icons/index.tsx
+++ src/assets/icons/index.tsx
@@ -5,3 +5,4 @@ export type Icon = React.FC<SvgProps>;
 export { default as CartArrowDown } from './cart-arrow-down.svg';
 export { default as CartArrowUp } from './cart-arrow-up.svg';
 export { default as CartShopping } from './cart-shopping.svg';
+export { default as Delete } from './delete.svg';
